b866fa44abb08411bb63eb70d266676d8fb0c3f2
Alex N <alex.ngai@gmail.com>
2020-04-28 16:31:42 +0900

    Working clear button

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,11 +1,14 @@
 const container = document.querySelector('#container');
+let containerSize = 480;    // Set container size
+container.style.width = containerSize + 'px';
 
-function createColumns (row) {
-    for (c = 1; c <= 16; c++) {
+function createColumns (row, size) {
+    let oneSquare = containerSize/size;
+    for (c = 1; c <= size; c++) {
             let col = document.createElement('div');
             col.style.display = 'inline-block';
-            col.style.width = '10px';
-            col.style.height = '10px';
+            col.style.width = oneSquare + 'px';
+            col.style.height = oneSquare + 'px';
             col.style.backgroundColor = 'blue';
             col.setAttribute("id", "grid");
             row.appendChild(col);
@@ -13,20 +16,38 @@ function createColumns (row) {
 
 }
 
-for (r = 1; r <= 16; r++) {
-    let row = document.createElement('div');
-    row.style.display = 'block';
-    row.style.fontSize = '0pt'; // Removes whitespace (vertical) from inline-block
+function createRows (size) {
+    for (r = 1; r <= size; r++) {
+        let row = document.createElement('div');
+        row.style.display = 'block';
+        row.style.fontSize = '0pt'; // Removes whitespace (vertical) from inline-block
 
-    createColumns(row);         // Create 16 columns
-    container.appendChild(row); // Create 16 rows
+        createColumns(row, size);         // Create columns
+        container.appendChild(row);       // Create rows
+    }
+    coloring();
 }
+
+createRows(16); // Initial grid
 
 function filling (e) {
     e.target.style.backgroundColor = 'black';
 }
 
-const divs = Array.from(document.querySelectorAll('#grid'));
-divs.forEach(div => div.addEventListener('mouseenter', filling));
-    // Fills in the grid square when mouse hovers over it
+function coloring (){
+    const divs = Array.from(document.querySelectorAll('#grid'));
+    divs.forEach(div => div.addEventListener('mouseenter', filling));
+        // Fills in the grid square when mouse hovers over it
+}
+
+function clear () {
+    while (container.hasChildNodes()) {     // Deletes old grid
+        container.removeChild(container.firstChild);
+    }
+    let size = prompt("Enter new grid size: ");
+    createRows(size);
+}
+
+const btn = document.querySelector('#clear');
+btn.addEventListener('click', clear);
 
